06d390ddd0fd9d9bf522c5c840f8929c4569514e
Cleiton barros <ton.barros123@hotmail.com>
2023-10-23 20:22:44 -0300

    :gear: feat: set-up  tailwind and eslint

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,66 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        suisse: [
+          "Suisse Intl",
+          "ui - sans - serif",
+          "system - ui",
+          "-apple - system",
+          "BlinkMacSystemFont",
+          "Segoe UI",
+          "Roboto",
+          "Helvetica Neue",
+          "Arial",
+          "Noto Sans",
+          "sans - serif",
+          "Apple Color Emoji",
+          "Segoe UI Emoji",
+          "Segoe UI Symbol",
+          "Noto Color Emoji"
+        ],
+        inter: ["Inter", "sans-serif"]
       },
-    },
+      colors: {
+        blue: {
+          light: "#00CFDE"
+        },
+        green: {
+          light: "#25D366"
+        },
+        rizer: {
+          50: "#9DBFF9",
+          100: "#5B8DEF",
+          200: "#0063F7",
+          800: "#032352",
+          900: "#01183B"
+        }
+      },
+      animation: {
+        scroll_reverse: "scroll_reverse 240s linear infinite",
+        scroll: "scroll 240s linear infinite"
+      },
+      keyframes: {
+        scroll: {
+          "0%": { transform: "translateX(0%)" },
+          "100%": { transform: "translateX(calc(-100% - 0rem))" }
+        },
+        scroll_reverse: {
+          "0%": { transform: "translateX(calc(-100% - 0rem))" },
+          "100%": { transform: "translateX(0%)" }
+        }
+      },
+      padding: {
+        "6.25": "100px",
+        "7.5": "120px"
+      }
+    }
   },
-  plugins: [],
-}
-export default config
+  plugins: []
+};
+export default config;
